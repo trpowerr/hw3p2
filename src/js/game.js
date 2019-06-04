1ace082c07873bf8cb46885cf0f2646cf9005093
trpowerr <mixazbubl@yandex.ru>
2019-06-04 17:20:58 +0300

    ver1

---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  import Character from './domain.js'
+
+  export default Game;
+
+  export { GameSavingData, readGameSaving, writeGameSaving };
+
